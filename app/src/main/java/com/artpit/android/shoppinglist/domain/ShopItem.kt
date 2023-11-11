42e44979c9f9cebc609bb924b383b5de7f2852f0
artpit <artpit.phone@gmail.com>
2023-11-11 11:23:01 +0200

    Data

--- app/src/main/java/com/artpit/android/shoppinglist/domain/ShopItem.kt
+++ app/src/main/java/com/artpit/android/shoppinglist/domain/ShopItem.kt
@@ -1,8 +1,12 @@
 package com.artpit.android.shoppinglist.domain
 
 data class ShopItem(
-    val id: Int,
     val name: String,
     val count: Int,
-    val enabled: Boolean
-)
+    val enabled: Boolean,
+    var id: Int = UNKNOWN_ID
+) {
+    companion object {
+        const val UNKNOWN_ID = -1
+    }
+}
